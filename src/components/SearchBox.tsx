a180d43553f0780cc1dcb3fa698dbbc06e22f284
rahul <rahul@gmail.com>
2024-03-06 18:59:36 +0530

    optimize svg icon

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,7 @@
 import { debounce } from "lodash"
 import React, { useEffect, useState } from "react"
+import SVGImage from "./SvgRender"
+import { SearchSvgIcon } from "../assets"
 
 interface iSearchBoxComponent {
   setPageData: React.Dispatch<any>
@@ -41,16 +43,7 @@ const SearchBoxComponent: React.FC<iSearchBoxComponent> = ({
           placeholder="Search"
         />
         <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4">
-          <svg
-            className="size-4 text-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-          </svg>
+          <SVGImage src={SearchSvgIcon} title="searchIcon" />
         </div>
       </div>
     </>
